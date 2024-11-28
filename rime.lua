1b8670b490747ca860e85b3921a7e62ba3e60663
abs2free <abs2free@gmail.com>
2024-11-28 19:42:58 +0800

    clean up
    clean up
    
    clean up
    
    clean up
    
    clean up

--- rime.lua
+++ rime.lua
@@ -1,16 +1,3 @@
--- select_character_processor: 以词定字
--- 详见 `lua/select_character.lua`
-
--- 需要以词定字插件打开第5行注释
--- select_character_processor = require("select_character")
-
-
--- https://github.com/hchunhui/librime-lua/blob/master/sample/lua/number.lua
-number = require("number")
-
--- https://github.com/hchunhui/librime-lua/issues/14
-thousand = require("thousand")
-
 -- https://github.com/yanhuacuo/98wubi-tables
 lunar = require("lunar")
 jieqi = require("jieqi")
